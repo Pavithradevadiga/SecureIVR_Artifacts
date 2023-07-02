9371627ec150e4c38302cab3c195c6e47eca3da3
pavithradevadiga <pavithradevadiga1@gmail.com>
2023-07-02 22:58:06 +0530

    updated lambda code

--- DecryptCustomerInputV2-17a48189-0b66-4971-ae50-8b0c88be0b7a/lambda_function.py
+++ DecryptCustomerInputV2-17a48189-0b66-4971-ae50-8b0c88be0b7a/lambda_function.py
@@ -1,69 +1,68 @@
-'''
-Requires these modules
-    pip install aws-encryption-sdk --target .
-    pip install cryptography --target .
-'''
-import boto3
+#!/usr/bin/env python
 import base64
-import aws_encryption_sdk
-from aws_encryption_sdk.key_providers.raw import RawMasterKeyProvider, WrappingKey
-from aws_encryption_sdk.identifiers import EncryptionKeyType, WrappingAlgorithm
 
-ssm = boto3.client('ssm')
+import boto3
+import requests
+import Crypto
+# from Crypto.Cipher import AES
 
-PARAM_PRIVATE_KEY = 'CONNECT_INPUT_DECRYPTION_KEY'
-PARAM_KEY_ID = 'CONNECT_INPUT_KEY_ID'
 
-class ConnectDecryption(RawMasterKeyProvider):
-    provider_id = "AmazonConnect"
+PAD = lambda s: s + (32 - len(s) % 32) * ' '
 
-    def __init__(self, **kwargs):
-        self._static_keys = {}
-        
-    def _get_raw_key(self, key_id):
-        self._static_keys[key_id] = self._privateKey
 
-        return WrappingKey(
-            wrapping_algorithm=WrappingAlgorithm.RSA_OAEP_SHA512_MGF1,
-            wrapping_key=self._privateKey,
-            wrapping_key_type=EncryptionKeyType.PRIVATE
-        )
-    
-    def setPrivateKey(self, key):
-        self._privateKey = key
+def get_arn(aws_data):
+    return 'arn:aws:kms:{region}:{account_number}:key/{key_id}'.format(**aws_data)
 
-def lambda_handler(event, context):
-    # print (event)
-    
-    encryptedText64 = event['Details']['ContactData']['Attributes']['EncryptedCreditCard']
-    encryptedText = base64.b64decode(encryptedText64)
 
-    response = ssm.get_parameter(
-        Name = PARAM_PRIVATE_KEY,
-        WithDecryption = True
-    )
-    privateKey = response['Parameter']['Value']
-    privateKeyBytes = bytearray(privateKey, 'utf8')
-    
-    response = ssm.get_parameter(
-        Name = PARAM_KEY_ID,
-        WithDecryption = True
-    )
-    keyId = response['Parameter']['Value']
-    
-    masterKeyProvider = ConnectDecryption()
-    masterKeyProvider.setPrivateKey(privateKeyBytes)
-    masterKeyProvider.add_master_key(keyId)
-    
-    plainTextBytes, decryptedHeader = aws_encryption_sdk.decrypt(
-        source=encryptedText,
-        key_provider=masterKeyProvider
-    )
-    plainText = plainTextBytes.decode('utf-8')
-    
-    print ('The value entered is ' + plainText)
-    
-    return {
-        'statusCode': 200,
-        'body': plainText
+def encrypt_data(aws_data, plaintext_message):
+    kms_client = boto3.client(
+        'kms',
+        region_name=aws_data['region'])
+
+    data_key = kms_client.generate_data_key(
+        KeyId=aws_data['key_id'],
+        KeySpec='AES_128')
+
+    cipher_text_blob = data_key.get('CiphertextBlob')
+    plaintext_key = data_key.get('Plaintext')
+
+    # Note, does not use IV or specify mode... for demo purposes only.
+    cypher = AES.new(plaintext_key)
+    encrypted_data = base64.b64encode(cypher.encrypt(PAD(plaintext_message)))
+
+    # Need to preserve both of these data elements
+    return encrypted_data, cipher_text_blob
+
+
+def decrypt_data(aws_data, encrypted_data, cipher_text_blob):
+    kms_client = boto3.client(
+        'kms',
+        region_name=aws_data['region'])
+
+    decrypted_key = kms_client.decrypt(CiphertextBlob=cipher_text_blob).get('Plaintext')
+    cypher = AES.new(decrypted_key)
+
+    return cypher.decrypt(base64.b64decode(encrypted_data)).rstrip()
+
+
+def main():
+    # Add your account number / region / KMS Key ID here.
+    aws_data = {
+        'region': 'us-east-1',
+        'account_number': '379419836112',
+        'key_id': 'b9296038-f9e2-494d-b657-7202a671cb15',
     }
+
+    # And your super secret message to envelope encrypt...
+    plaintext = 'Hello, World!'
+
+    # Store encrypted_data & cipher_text_blob in your persistent storage. You will need them both later.
+    encrypted_data, cipher_text_blob = encrypt_data(aws_data, plaintext)
+    print(encrypted_data)
+
+    # # Later on when you need to decrypt, get these from your persistent storage.
+    decrypted_data = decrypt_data(aws_data, encrypted_data, cipher_text_blob)
+    print(decrypted_data)
+
+if __name__ == '__main__':
+    main()
